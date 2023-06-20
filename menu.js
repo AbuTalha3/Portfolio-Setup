269bffa146a984d2d0ab9f90e06699728118227e
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-06-21 01:02:09 +0500

    Linter errors resolved

--- menu.js
+++ menu.js
@@ -1,21 +1,20 @@
-
-const menu = document.querySelector(".mobile");
-const closeicons = document.querySelector(".close");
-const hamburger = document.querySelector(".menubars");
+const menu = document.querySelector('.mobile');
+const closeicons = document.querySelector('.close');
+const hamburger = document.querySelector('.menubars');
 const contentMenu = document.querySelector('.content_menu');
 
 const showMenu = () => {
-  menu.classList.toggle('active')
+  menu.classList.toggle('active');
 };
 
 const hideMenu = () => {
-  menu.classList.toggle('active')
+  menu.classList.toggle('active');
 };
 
 const contentMenuFunc = () => {
-  menu.classList.toggle('active')
+  menu.classList.toggle('active');
 };
 
-hamburger.addEventListener("click", showMenu);
-contentMenu.addEventListener("click", contentMenuFunc);
-closeicons.addEventListener("click", hideMenu);
+hamburger.addEventListener('click', showMenu);
+contentMenu.addEventListener('click', contentMenuFunc);
+closeicons.addEventListener('click', hideMenu);
