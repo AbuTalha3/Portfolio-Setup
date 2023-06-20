fdbbf90ad906e967f7c3e3c46a8985661d484f37
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-06-20 20:51:39 +0100

    add some code

--- menu.js
+++ menu.js
@@ -31,19 +31,53 @@ const theList =[
     source: "See-source",
     IconSource: '<i class="fa-brands fa-github"></i>',
     IconLive: '<i class="fa-solid fa-arrow-up-right-from-square"></i>',
+  },
+  { id:1,
+    name:"Tonic",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    image:"images/card2.png",
+    portfolioLive:"https://abutalha3.github.io/Portfolio-Setup/",
+    softLinkToRepo: "https://github.com/AbuTalha3/Portfolio-Setup",
+    live:"see live",
+    source: "See-source",
+    IconSource: '<i class="fa-brands fa-github"></i>',
+    IconLive: '<i class="fa-solid fa-arrow-up-right-from-square"></i>',
+  },
+  { id:1,
+    name:"Tonic",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    image:"./images/card3img.png",
+    portfolioLive:"https://abutalha3.github.io/Portfolio-Setup/",
+    softLinkToRepo: "https://github.com/AbuTalha3/Portfolio-Setup",
+    live:"see live",
+    source: "See-source",
+    IconSource: '<i class="fa-brands fa-github"></i>',
+    IconLive: '<i class="fa-solid fa-arrow-up-right-from-square"></i>',
+  },
+  { id:1,
+    name:"Tonic",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    image:"./images/card4img.png",
+    portfolioLive:"https://abutalha3.github.io/Portfolio-Setup/",
+    softLinkToRepo: "https://github.com/AbuTalha3/Portfolio-Setup",
+    live:"see live",
+    source: "See-source",
+    IconSource: '<i class="fa-brands fa-github"></i>',
+    IconLive: '<i class="fa-solid fa-arrow-up-right-from-square"></i>',
   }
 ]
 
-document.getElementById('cardone') = theList.map((item)=>{
+document.getElementById('portfolio').innerHTML = theList.map((item)=>{
   return`
+  <div id="cardone" class="card">
   <div class="card-image">
-            <img class="SSimage" src="${Item.image}"
+            <img class="SSimage" src="${item.image}"
               alt="A school dropout detail who became a web designer at facebook">
-            <img class="BSimage" src="./images/TonicCard1gibscreen.png"
+            <img class="BSimage" src="${item.image}"
               alt="An image with a tenis ball, a swiming pool ladder and a decorative plant">
           </div>
           <div class="card-text">
-            <h3>Tonic</h3>
+            <h3>${item.name}</h3>
             <div class="expertise">
               <ul>
                 <li>CANOPY</li>
@@ -53,7 +87,7 @@ document.getElementById('cardone') = theList.map((item)=>{
                 <li>2015</li>
               </ul>
             </div>
-            <p>A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
+            <p>${item.description}</p>
             <div class="programes">
               <ul>
                 <li>HTML</li>
@@ -63,5 +97,7 @@ document.getElementById('cardone') = theList.map((item)=>{
             </div>
             <button>See project</button>
           </div>
+          </div>
+          
   `
 })
