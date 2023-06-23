a45339b2cc11f1c0d430e295f3f342e514c280fd
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-06-23 09:32:38 +0500

    Update Validation Form

---
+++ form.js
@@ -0,0 +1,12 @@
+const email = document.getElementById('email');
+function message(errorMessage) {
+  document.getElementById('message2').innerHTML = errorMessage;
+}
+function validation(event) {
+  if (email.value !== email.value.toLowerCase()) {
+    message('email should be in lowercase');
+    event.preventDefault;
+    return true;
+  }
+  return false;
+}
