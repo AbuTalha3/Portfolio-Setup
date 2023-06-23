406888c33380c63d3347902a36d373f848d3b0ee
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-06-23 12:19:46 +0500

    Updated form.js file

--- form.js
+++ form.js
@@ -1,13 +1,29 @@
 /* eslint-disable no-unused-vars */
-const email = document.getElementById('email');
-function message(errorMessage) {
-  document.getElementById('message2').innerHTML = errorMessage;
-}
-function validation(event) {
-  if (email.value !== email.value.toLowerCase()) {
-    message('email should be in lowercase');
-    event.preventDefault;
-    return true;
-  }
-  return false;
-}
+// const email = document.getElementById('email');
+// function message(errorMessage) {
+//   document.getElementById('message2').innerHTML = errorMessage;
+// }
+// function validation(event) {
+//     if (email.value !== email.value.toLowerCase()) {
+//         message('email should be in lowercase');
+//         event.preventDefault();
+//     return true;
+//   }
+//   return false;
+// }
+
+const form = document.getElementById('error2');
+const email = document.getElementById('email').value;
+const correctEmail = email.toLowerCase();
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    if(email === correctEmail){
+        form.submit()
+    }else{
+        const errorMessage = document.getElementById('message2')
+        errorMessage.textContent = "email should be in lowercase"
+        errorMessage.style.display = "block"
+    }
+})
+
