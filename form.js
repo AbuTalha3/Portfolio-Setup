4d122952900e9eeac29ab82c9f5b07d2120975b5
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-06-23 17:33:03 +0500

    Removed errors of linter

--- form.js
+++ form.js
@@ -1,20 +1,5 @@
 /* eslint-disable no-unused-vars */
-// const email = document.getElementById('email');
-// function message(errorMessage) {
-//   document.getElementById('message2').innerHTML = errorMessage;
-// }
-// function validation(event) {
-//     if (email.value !== email.value.toLowerCase()) {
-//         message('email should be in lowercase');
-//         event.preventDefault();
-//     return true;
-//   }
-//   return false;
-// }
-
 const form = document.getElementById('error2');
-
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const email = document.getElementById('email').value;
