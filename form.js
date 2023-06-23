36cfa1460537e57984b4fe514baacd2cae4d887d
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-06-23 12:22:44 +0500

    Removed linter errors

--- form.js
+++ form.js
@@ -16,14 +16,13 @@ const form = document.getElementById('error2');
 const email = document.getElementById('email').value;
 const correctEmail = email.toLowerCase();
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    if(email === correctEmail){
-        form.submit()
-    }else{
-        const errorMessage = document.getElementById('message2')
-        errorMessage.textContent = "email should be in lowercase"
-        errorMessage.style.display = "block"
-    }
-})
-
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (email === correctEmail) {
+    form.submit();
+  } else {
+    const errorMessage = document.getElementById('message2');
+    errorMessage.textContent = 'email should be in lowercase';
+    errorMessage.style.display = 'block';
+  }
+});
